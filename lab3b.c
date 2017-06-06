05c4bf39e5c540fc9908e94bd2e4da49129a9cec
= <=>
2017-06-05 23:39:36 -0700

    hmm its a start :/

--- lab3b.c
+++ lab3b.c
@@ -14,10 +14,21 @@ int csvFileSize;
 int numBlocks, numInodes;
 int reservedBlockMax;
 
-char * blockStatus;
+struct allocatedBlock {
+    unsigned int blockNum;
+    unsigned int parentInode;
+    unsigned int offset;
+    unsigned int level;
+    unsigned int isDuplicated;
+};
+
+int * freeBlocks;
+struct allocatedBlock * allocatedBlocks;
+
 char * inodeStatus;
 
 int fd;
+
 
 
 
@@ -26,8 +37,8 @@ void freeMemory()
 {
   if (csv != NULL)
     free(csv);
-  if (blockStatus != NULL)
-    free(blockStatus);
+  if (freeBlocks != NULL)
+    free(freeBlocks);
   if (inodeStatus != NULL)
     free(inodeStatus);
   //  free(superBlock);
@@ -58,10 +69,12 @@ void getSuperblockInfo(char* str) {
 int isValidBlockNum(int block) {
     if(block < numBlocks && block > reservedBlockMax)
         return 1;
-    else return 0;
+    else if (block <= reservedBlockMax && block > 0)
+        return 0; //reserved block
+    else return -1; //invalid block
 }
 
-int isValidInodeNum(int inum) {
+int isValidInodeNum(int num) {
   if(num <= numInodes && num > 0)
     return 1;
   else return 0;
@@ -74,14 +87,128 @@ void getBlockFreeListInfo(char* str) {
     if (num == 2)
       {
 	int blockNum = atoi(tok);
-	if(isValidBlockNum(blockNum))
-	  blockStatus[blockNum] = 'F';
-	else
-	  printf("INVALID BLOCK %d IN FREE LIST\n", blockNum);
-		
+    int rc = isValidBlockNum(blockNum);
+	if (rc == 1)
+	  freeBlocks[blockNum] = 1;
+	else if (rc == 0)
+	  fprintf(stdout, "RESERVED BLOCK %d IN FREE LIST\n", blockNum);
+    else
+        fprintf(stdout, "INVALID BLOCK %d IN FREE LIST\n", blockNum);
+
       }
   }
 }
+
+char* indirect = "INDIRECT ";
+char* doubleIndirect = "DOUBLE INDIRECT ";
+char* trippleIndirect = "TRIPPLE INDIRECT ";
+char* emptyString = "";
+
+char* getIndirection (unsigned int offset)
+{
+
+    switch (offset) {
+        case 1:
+        case 12: return indirect;
+        case 2:
+        case 13: return doubleIndirect;
+        case 14: return trippleIndirect;
+        default: return emptyString;
+    }
+}
+
+void getInodeInfo(char* str) {
+    
+    char* tok = strtok(str, ",");
+
+    int num = 1, inodeNum = 0, blockNum = 0;
+    for (; num < 13; num++) {
+        if (num == 2) inodeNum = atoi(tok);
+        tok = strtok(NULL, ",");
+    }
+    
+    int offset = 0;
+    char * indirect = "";
+    
+    for (; tok != NULL; tok = strtok(NULL, ","), offset++) {
+        indirect = getIndirection(offset);
+        blockNum = atoi(tok);
+        if (blockNum == 0)
+            continue;
+        int rc = isValidBlockNum(blockNum);
+        if (rc == 1)
+        {
+            if(allocatedBlocks[blockNum].blockNum != 0)
+            {
+                if(allocatedBlocks[blockNum].isDuplicated == 0)
+                {
+                    allocatedBlocks[blockNum].isDuplicated = 1;
+                    fprintf(stdout, "DUPLICATE %sBLOCK %u IN INODE %u AT OFFSET %u\n", indirect, allocatedBlocks[blockNum].blockNum, allocatedBlocks[blockNum].parentInode, allocatedBlocks[blockNum].offset);
+                }
+                fprintf(stdout, "DUPLICATE %sBLOCK %u IN INODE %u AT OFFSET %u\n", indirect, blockNum, inodeNum, offset);
+            }
+            else
+            {
+                allocatedBlocks[blockNum].blockNum = blockNum;
+                allocatedBlocks[blockNum].parentInode = inodeNum;
+                allocatedBlocks[blockNum].offset = offset;
+                switch (offset) {
+                    case 12: allocatedBlocks[blockNum].level = 1; break;
+                    case 13: allocatedBlocks[blockNum].level = 2; break;
+                    case 14: allocatedBlocks[blockNum].level = 3; break;
+                    default: allocatedBlocks[blockNum].level = 0; break;
+                }
+            }
+        }
+        else if(rc == 0)
+            fprintf(stdout, "RESERVED %sBLOCK %d IN INODE %d AT OFFSET %d\n", indirect, blockNum, inodeNum, offset);
+        else
+            fprintf(stdout, "INVALID %sBLOCK %d IN INODE %d AT OFFSET %d\n", indirect, blockNum, inodeNum, offset);
+    }
+    
+}
+
+void getIndirectBlock(char *str) {
+    char * tok = strtok(str, ",");
+
+    int num = 1, inodeNum = 0, offset = 0, referencedBlockNum = 0, level = 0;
+    for (; tok != NULL; tok = strtok(NULL, ","), num++) {
+        if (num == 2) inodeNum = atoi(tok);
+        else if (num == 3) level = atoi(tok);
+        else if (num == 4) offset = atoi(tok);
+        else if (num == 6) referencedBlockNum = atoi(tok);
+    }
+    
+    char * indirect = getIndirection(level - 1);
+    int rc = isValidBlockNum(referencedBlockNum);
+    if (rc == 1)
+    {
+        if(allocatedBlocks[referencedBlockNum].blockNum != 0)
+        {
+            if(allocatedBlocks[referencedBlockNum].isDuplicated == 0)
+            {
+                allocatedBlocks[referencedBlockNum].isDuplicated = 1;
+                char* thisIndirect = getIndirection(allocatedBlocks[referencedBlockNum].level);
+                fprintf(stdout, "DUPLICATE %sBLOCK %u IN INODE %u AT OFFSET %u\n", thisIndirect, allocatedBlocks[referencedBlockNum].blockNum, allocatedBlocks[referencedBlockNum].parentInode, allocatedBlocks[referencedBlockNum].offset);
+            }
+            fprintf(stdout, "DUPLICATE %sBLOCK %u IN INODE %u AT OFFSET %u\n", indirect, referencedBlockNum, inodeNum, offset);
+        }
+        else
+        {
+            allocatedBlocks[referencedBlockNum].blockNum = referencedBlockNum;
+            allocatedBlocks[referencedBlockNum].parentInode = inodeNum;
+            allocatedBlocks[referencedBlockNum].offset = offset;
+            allocatedBlocks[referencedBlockNum].level = level - 1;
+        }
+    }
+    else if(rc == 0)
+        fprintf(stdout, "RESERVED %sBLOCK %d IN INODE %d AT OFFSET %d\n", indirect, referencedBlockNum, inodeNum, offset);
+    else
+        fprintf(stdout, "INVALID %sBLOCK %d IN INODE %d AT OFFSET %d\n", indirect, referencedBlockNum, inodeNum, offset);
+}
+
+
+
 
 void getInodeFreeListInfo(char* str) {
     char* tok = strtok(str, ",");
@@ -98,64 +225,28 @@ void getInodeFreeListInfo(char* str) {
 }
 
 
-void checkBlockStatus(int blockNum) {
+/*void checkBlockStatus(int blockNum) {
     // Mark the block as allocated in our dictionary unless it is supposed to be free
-    if (blockStatus[blockNum] == 'F')
+    if (freeBlocks[blockNum] == 1)
       fprintf(stdout, "ALLOCATED BLOCK %d ON FREELIST\n", blockNum);
     else
         blockStatus[blockNum] = 'A';
 }
-
+*/
 
 void testDirectBlocksInInode(char* str) {
     char * tok = strtok(str, ",");
 
-  int num = 1, inodeNum = 0, blockNum = 0;
-  for (; num < 13; num++) {
-      if (num == 2) inodeNum = atoi(tok);
-      tok = strtok(NULL, ",");
   }
-
-  int offset = 0;
-  for (; tok != NULL; tok = strtok(NULL, ","), offset++) {
-    blockNum = atoi(tok);
-    checkBlockStatus(blockNum);
-    if (blockNum < 0 || blockNum > numBlocks - 1 || inodeNum < 1 || inodeNum > numInodes)
-      fprintf(stdout, "INVALID BLOCK %d IN INODE %d AT OFFSET %d\n", blockNum, inodeNum, offset);
-  }
-}
-
-void testIndirectBlocks(char* str) {
-    char * tok = strtok(str, ",");
-
-  int num = 1, inodeNum = 0, offset = 0, referencedBlockNum = 0, level = 0;
-  for (; num < 13; num++) {
-      if (num == 2) inodeNum = atoi(tok);
-      else if (num == 3) level = atoi(tok);
-      else if (num == 4) offset = atoi(tok);
-      else if (num == 6) referencedBlockNum = atoi(tok);
-      tok = strtok(NULL, ",");
-  }
-
-  checkBlockStatus(referencedBlockNum);
-
-  char * levelDesc;
-  switch (level) {
-      case 1: levelDesc = ""; break;
-      case 2: levelDesc = "DOUBLE "; break;
-      case 3: levelDesc = "TRIPLE "; break;
-  }
-  if (referencedBlockNum < 0 || referencedBlockNum > numBlocks - 1 || inodeNum < 1 || inodeNum > numInodes)
-    fprintf(stdout, "INVALID %s INDIRECT BLOCK %d IN INODE %d AT OFFSET %d\n", levelDesc, referencedBlockNum, inodeNum, offset);
-}
 
 void testUnreferencedBlocks() {
     int i;
     for (i = 0; i < numBlocks; i++) {
-        if (blockStatus[i] != 'F' && blockStatus[i] != 'A')
+        if (freeBlocks[i] == 0 && allocatedBlocks[i].blockNum == 0 && i > reservedBlockMax)
             fprintf(stdout, "UNREFERENCED BLOCK %d\n", i);
     }
 }
+
 
 int
 main (int argc, char **argv)
@@ -167,7 +258,7 @@ main (int argc, char **argv)
   }
 
   numBlocks = -1, numInodes = -1;
-  csv = NULL, blockStatus = NULL, inodeStatus = NULL;
+  csv = NULL, allocatedBlocks = NULL, freeBlocks = NULL, inodeStatus = NULL;
 
   //Check to see if we can open provided csv
   char * csvFile = argv[1];
@@ -180,11 +271,15 @@ main (int argc, char **argv)
       if (strcmp(pch, "SUPERBLOCK") == 0) {
         getSuperblockInfo(line);
         if (numBlocks > -1)
-	  blockStatus = (char*) malloc(sizeof(char) * (numBlocks-1));
+        {
+            freeBlocks = (int*) malloc(sizeof(int) * (numBlocks));
+            memset(freeBlocks, 0, sizeof(int) * numBlocks);
+            allocatedBlocks = (struct allocatedBlock *) malloc(sizeof(struct allocatedBlock) * numBlocks);
+            memset(allocatedBlocks, 0, sizeof(struct allocatedBlock) * numBlocks);
+        }
           
         int inodesPerBlock = BLOCK_SIZE/INODE_SIZE;
         int inodeTableBlocks = numInodes / inodesPerBlock;
-	printf("%u\n", inodeTableBlocks);
 	reservedBlockMax = 4 + inodeTableBlocks;
     
 	inodeStatus = (char*) malloc(sizeof(char) * numInodes);
@@ -198,10 +293,10 @@ main (int argc, char **argv)
       }
       else if (strcmp(pch, "INODE") == 0) {
           // the direct block info is all in the inode rows
-        testDirectBlocksInInode(line);
+        getInodeInfo(line);
       }
       else if (strcmp(pch, "INDIRECT") == 0) {
-        testIndirectBlocks(line);
+        getIndirectBlock(line);
       }
       free(temp);
   }
